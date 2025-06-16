bbeb38c37ade4d806c1b55345136c13b60442b24
lee-to <lee-to@users.noreply.github.com>
2025-06-16 13:55:36 +0000

    Fix styling

--- src/Core/CrudResourceWithResponseModifiersContract.php
+++ src/Core/CrudResourceWithResponseModifiersContract.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace MoonShine\Contracts\Core;
 
-use MoonShine\Contracts\Core\TypeCasts\DataWrapperContract;
 use Illuminate\Contracts\Support\Jsonable;
+use MoonShine\Contracts\Core\TypeCasts\DataWrapperContract;
 
 /**
  * @internal
